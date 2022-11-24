3ae9cc3c50e92e55bf2d6150bd50e04cb9d92aa9
Martin Ström <martin@datanist.se>
2022-11-24 22:11:18 +0100

    realtime with notifications working

--- src/lib/stores/messages.ts
+++ src/lib/stores/messages.ts
@@ -1,5 +1,6 @@
 import { getUnreadMessageCount } from '$lib/core/api';
 import supabase from '$lib/core/supabase';
+import Notify from 'simple-notify';
 import { writable } from 'svelte/store';
 
 export let unreadMessages = writable(0);
@@ -16,8 +17,25 @@ export async function subscribeToMessages(username: string) {
 		.channel('messages')
 		.on('postgres_changes', { event: '*', schema: '*' }, async (payload: any) => {
 			if (payload.new.recipient == _username) {
-				console.log('hit!');
 				unreadMessages.set((await getUnreadMessageCount(_username)) as number);
+				new Notify({
+					status: 'success',
+					title: `${payload.new.sender} sent you a message!`,
+					text:
+						payload.new.content.length > 20
+							? `${payload.new.content.slice(0, 20)}...`
+							: `${payload.new.content}`,
+					effect: 'fade',
+					speed: 300,
+					showIcon: true,
+					showCloseButton: true,
+					autoclose: true,
+					autotimeout: 3000,
+					gap: 20,
+					distance: 20,
+					type: 1,
+					position: 'right bottom'
+				});
 			}
 		})
 		.subscribe();
