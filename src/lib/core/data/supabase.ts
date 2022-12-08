721b0e281ffe5bb6e5abfe6713bb91c7a5c5d0ea
Martin Ström <martin@datanist.se>
2022-12-08 22:00:52 +0100

    minor refactoring

--- src/lib/core/data/supabase.ts
+++ src/lib/core/data/supabase.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/auth-helpers-sveltekit';
+import { createClient, getSupabase } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
-
+import {} from '@supabase/auth-helpers-sveltekit';
+import type { RequestEvent } from '.svelte-kit/types/src/routes/(auth)/$types';
 const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
 
 export default supabase;
+
+
